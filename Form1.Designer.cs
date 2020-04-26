0f51db6f0bcd9c400df42f79eadbf4ab8ac6ce55
George Njubi <george.njubi@outlook.com>
2020-04-26 12:09:41 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Calculate_income_tax
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.txtIncome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Taxable Income:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Income Tax Owed:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(48, 194);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(136, 52);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(208, 194);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(136, 52);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtTax
+            // 
+            this.txtTax.Location = new System.Drawing.Point(200, 104);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.ReadOnly = true;
+            this.txtTax.Size = new System.Drawing.Size(137, 22);
+            this.txtTax.TabIndex = 0;
+            this.txtTax.TabStop = false;
+            // 
+            // txtIncome
+            // 
+            this.txtIncome.Location = new System.Drawing.Point(200, 48);
+            this.txtIncome.Name = "txtIncome";
+            this.txtIncome.Size = new System.Drawing.Size(137, 22);
+            this.txtIncome.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(407, 295);
+            this.Controls.Add(this.txtIncome);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.TextBox txtIncome;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculate_income_tax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+            //George Njubi
+            //04/26/2020
+            //Exercise 5-3 Calculate Income Tax
+        {
+            decimal TaxableIncome = Convert.ToDecimal(txtIncome.Text);
+            decimal IncomeTaxOwed = 0m;
+
+
+            if (TaxableIncome <= 9225)
+                IncomeTaxOwed = (int)(TaxableIncome * .1m);
+
+            //$9225 - $37450
+            else if (TaxableIncome > 9225 && TaxableIncome <= 37450)
+                IncomeTaxOwed = 922.50m + (int)((TaxableIncome - 9225) * .15m);
+
+            //37450 - 90750
+            else if (TaxableIncome > 37450 && TaxableIncome <= 90750)
+                IncomeTaxOwed = 5156.25m + (int)((TaxableIncome - 37450) * .25m);
+
+            //90750 - 189300
+            else if (TaxableIncome > 90750 && TaxableIncome <= 189300)
+                IncomeTaxOwed = 18481.25m + (int)((TaxableIncome - 90750) * .28m);
+
+            //189300 - 411500
+            else if (TaxableIncome > 189300 && TaxableIncome <= 411500)
+                IncomeTaxOwed = 46075.25m + (int)((TaxableIncome - 189300) * .33m);
+
+            //411500 - 413200
+            else if (TaxableIncome > 411500 && TaxableIncome <= 413200)
+                IncomeTaxOwed = 119401.25m + (int)((TaxableIncome - 411500) * .35m);
+
+            //413200 and above
+            else
+                IncomeTaxOwed = 119996.25m + (int)((TaxableIncome - 413200) * .396m);
+
+            //Writing calculated tax to form  
+            txtTax.Text = IncomeTaxOwed.ToString();
+
+        }
+    }
+}
